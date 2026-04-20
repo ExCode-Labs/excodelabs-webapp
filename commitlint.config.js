1265e26616286bcbfc77f06ee721a6335959e38b
Rahul Kumar <rahul.kumar@excodelabs.com>
2026-04-20 22:15:50 +0530

    chore: setup eslint, prettier, husky, commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'ci',
+      ],
+    ],
+    'type-case': [2, 'always', 'lowercase'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
